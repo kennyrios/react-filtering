03a6107e867d02b0f59dba96de26e2ffc31f8efd
kennyriosavila <karios93@misena.edu.co>
2022-05-16 19:31:02 -0700

    React filtering project practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+
+import {Component } from 'react'
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [searchField, setSearchField] = useState('a');//[value, setValue]
+  const [monsters, setMonsters] = useState([]);
+
+  console.log('render');
+
+
+  useEffect(() =>  {
+    console.log('effect fire');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, [])
+
+  const onSearchChange = (event)=> {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+    
+  }
+
+  const filteredMonster = monsters.filter((monster) =>{
+    return monster.name.toLocaleLowerCase().includes(searchField);
+  })
+
+    return (
+      <div className="App">
+       
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox
+          className = 'monsters-search-box' 
+          onChangeHandler={onSearchChange} 
+          placeholder='search monster' 
+        />
+        <CardList monsters={filteredMonster}/>
+      </div>
+    );
+  } 
+
 
 export default App;
